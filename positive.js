8eb9f80de546a4b4861828463d9a209ba6cecbb6
Joyel <joyelmathew806@gmail.com>
2025-02-07 10:38:05 +0530

    test case addded

--- positive.js
+++ positive.js
@@ -12,3 +12,18 @@ function checkNumber(num){
 console.log(checkNumber(1));
 console.log(checkNumber(0));
 console.log(checkNumber(-8));
+
+//test case
+
+function testCase(){
+    const tests = [
+        {input: 5, expected: "5 is a positive number"},
+        {input: -3, expected: "-3 is a positive number"},
+    ];
+    tests.forEach(({ input, expected}, index) => {
+        console.log(
+            `Test ${index + 1}: ${checkNumber(input) === expected ? "Passed" : "Failed"}`
+        );
+    });
+}
+testCase();
--- prime.js
+++ prime.js
@@ -12,3 +12,19 @@ function isPrime(num) {
     return "prime";
 }
 console.log(isPrime(71));
+
+//test case
+
+function testCase(){
+    const tests =[
+        {input: 2, expected: "prime"},
+        {input: 3, expected: "prime"},
+        {input: 4, expected: " prime"},
+        {input: 10, expected: "not prime"},
+    ];
+    tests.forEach(({input, expected}, index) => {
+        console.log(`Test ${index + 1}: ${isPrime(input) === expected ? "    Passed" : "Failed"}`);
+    });
+}
+
+testCase();
