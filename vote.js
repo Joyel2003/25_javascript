1515538c1dc10546f550208980cf80b723660405
Joyel <joyelmathew806@gmail.com>
2025-02-08 11:46:02 +0530

    test case added

--- vote.js
+++ vote.js
@@ -10,3 +10,17 @@ function checkEligibility(age){
 console.log(checkEligibility(20));
 console.log(checkEligibility(5));
 
+//test case
+function testCheckEligibility(){
+    let testCases = [
+        {input: 20, expected: "Eligible for voting"},
+        {input: 5, expected: " Not eligible for voting"},
+        {input: 18, expected: "Eligible for voting"},
+    ];
+    testCases.forEach(({input, expected},  index) => {
+        let result = checkEligibility(input);
+        console.log(`Test Case ${index + 1}:`, result === expected ? "Passed " : `Failed`);
+    });
+}
+
+testCheckEligibility();
