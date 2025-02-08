f32610e7f7f3fcdb1ce7096b26324134cabdcb37
Joyel <joyelmathew806@gmail.com>
2025-02-08 11:55:54 +0530

    test case added

--- vowels.js
+++ vowels.js
@@ -12,3 +12,20 @@ function countVowels(str){
     return count;
 }
 console.log(countVowels("I'm from kerala!"));
+
+//test case
+
+function testCountVowels(){
+    let testCases =[
+        {input: "I'm from Kerala!", expected: 6},
+        {input: "HELLO WORLD", expected: 3},
+        {input: "rhythm", expected: 0},
+        {input: "1234567890", expected: 0},
+    ];
+
+    testCases.forEach(({ input, expected}, index) => {
+        let result = countVowels(input);
+        console.log(`Test Case ${index + 1}:`, result === expected ? "Passed " : `Failed`);
+    });
+}
+testCountVowels();
